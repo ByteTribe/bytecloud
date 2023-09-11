3a31710ec086579de871f27d58d50f226eee11f3
Marcao <m.pereira@acad.ifma.edu.br>
2023-09-11 16:12:25 -0300

    feat: upload route with compress image

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,5 @@
 import supabase from "@/config/supabase";
+import sharp, { Metadata } from "sharp";
 import { v4 } from "uuid";
 
 const CDNURL = process.env.NEXT_PUBLIC_SUPABASE_CDNURL;
@@ -13,22 +14,96 @@ interface IResponse {
   fileName: string;
 }
 
+interface ext {
+  jpeg: string;
+  png: string;
+  webp: string;
+}
+
+const compressImage = async (file: File) => {
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  const image = sharp(buffer);
+
+  const fileType = file.type.split("/").pop() as keyof ext;
+
+  if (fileType === "jpeg" || fileType === "webp") {
+    return sharp(buffer)
+      .metadata()
+      .then(({ width }) => {
+        if (width) {
+          return image[fileType]({
+            quality: 60,
+            force: true,
+            progressive: true,
+            mozjpeg: true,
+          })
+            .resize(920, 500, {
+              kernel: sharp.kernel.nearest,
+              fit: "cover",
+              position: "center",
+            })
+            .toBuffer();
+        }
+      });
+  } else if (fileType === "png") {
+    return sharp(buffer)
+      .metadata()
+      .then(({ width }) => {
+        if (width) {
+          return image
+            .png({
+              quality: 60,
+              force: true,
+              progressive: true,
+            })
+            .resize(920, 500, {
+              kernel: sharp.kernel.nearest,
+              fit: "cover",
+              position: "center",
+            })
+            .toBuffer();
+        }
+      });
+  }
+};
+
 export const uploadFile = async ({
   file,
   contentType,
 }: IUpload): Promise<IResponse> => {
+  let compressedFile;
+  const imageTypes = ["image/jpg", "image/jpeg", "image/webp", "image/png"];
+
+  if (imageTypes.includes(file.type)) {
+    compressedFile = await compressImage(file);
+  }
+
   const id = v4();
+  if (compressedFile) {
+    const { error } = await supabase.storage
+      .from("thinktalk")
+      .upload(`/uploads/${id}`, compressedFile, {
+        contentType,
+      });
+
+    if (error) throw error;
+    return {
+      url: `${CDNURL}/uploads/${id}`,
+      fileName: id,
+    };
+  }
+
   const { error } = await supabase.storage
     .from("thinktalk")
     .upload(`/uploads/${id}`, file, {
       contentType,
     });
 
-  if (error) console.log(error);
+  if (error) throw error;
 
   return {
     url: `${CDNURL}/uploads/${id}`,
     fileName: id,
   };
 };
-
