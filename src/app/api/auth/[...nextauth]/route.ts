10c7f62ab25f000efd8e1e44017357c73d028aa0
Cayke Daniel <contato.cayke@gmail.com>
2023-09-13 20:20:39 -0300

    feat: implement auth communication with supabase

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
  
@@ -8,6 +10,10 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET??""
         }),
     ],
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL??"",
+        secret: process.env.SUPABASE_SERVICE_ROLE??""
+    }) as Adapter,
 });
 
 export { handler as GET, handler as POST };
